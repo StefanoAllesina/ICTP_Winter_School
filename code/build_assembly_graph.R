8726897fe04433b1f40d9dc4593bf8df54d384d3
Stefano Allesina <sallesina@uchicago.edu>
2020-12-01 17:58:55 -0600

    first part of lecture 3

--- code/build_assembly_graph.R
+++ code/build_assembly_graph.R
@@ -1,10 +1,13 @@
-A <- -matrix(runif(100), 10, 10)
+source("general_code.R")
+source("L-H.R")
+n <- 5
+A <- -matrix(runif(n * n), n, n)
+# make symmetric
 A <- A + t(A)
-r <- runif(10)
+# positive growth rates
+r <- runif(n)
 
-# cycle through all possible compositions and check whether it is a node in the graph
 n <- nrow(A)
-states <- tibble(label = 0, state = list(rep(0, n)))
 edges <- tibble(from = numeric(0), to = numeric(0))
 for (i in 1:(2^n - 1)){
   label <- i
@@ -13,7 +16,7 @@ for (i in 1:(2^n - 1)){
   if (sum(state) == 1){
     my_spp <- which(state == 1)
     if ((r[my_spp] > 0) & (A[my_spp, my_spp] < 0)){
-      states <- bind_rows(states, tibble(label = i, state = list(state)))
+      edges <- bind_rows(edges, tibble(from = 0, to = i))
       edges <- bind_rows(edges, tibble(from = i, to = i))
     } else {
       edges <- bind_rows(edges, tibble(from = i, to = 0))
@@ -23,9 +26,9 @@ for (i in 1:(2^n - 1)){
     Ared <- A[present, present, drop = FALSE]
     rred <- r[present]
     to_state <- (get_final_composition(Ared, rred) > 0) * 1
-    if (all(to_state > 0)){
-      states <- bind_rows(states, tibble(label = i, state = list(state)))
-    }
     edges <- bind_rows(edges, tibble(from = i, to = sum(2^to_state)))
   }
 }
+
+# for each node, determine who is one invasion above
+
